6f8b49cc022133aa981d9bcd03aec7a0f19114f5
NivMoshe2000 <niv.moshe@campus.technion.ac.il>
2023-01-17 16:38:34 +0200

    train test split function

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.feature_selection import chi2
+import os
+
+train_path = 'data/train.csv'
+test_path = 'data/test.csv'
+
+
+def create_train_test_files():
+    if not (os.path.exists(train_path) and os.path.exists(test_path)):
+        print('Creating Train and Test files')
+        # create train test split
+        titanic = pd.read_csv('data/titanic.csv')
+        train, test = train_test_split(titanic, test_size=0.2, random_state=42)
+        train.to_csv('data/train.csv', index=False)
+        test.to_csv('data/test.csv', index=False)
+    else:
+        print('Train and Test files already exist')
 
 
 def main():
+    create_train_test_files()
     pass
 
 
 if __name__ == "__main__":
-    # create train test split
-    titanic = pd.read_csv('data/titanic.csv')
-    train, test = train_test_split(titanic, test_size=0.2, random_state=42)
-    train.to_csv('data/train.csv', index=False)
-    test.to_csv('data/test.csv', index=False)
-
+    main()
     pass
