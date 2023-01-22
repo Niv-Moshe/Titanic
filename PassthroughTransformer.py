7190e20cef68880923a799e9d3a31b54244823ca
NivMoshe2000 <niv.moshe@campus.technion.ac.il>
2023-01-22 14:50:08 +0200

    PassthroughTransformer instead of remainder='passthrough'

--- PassthroughTransformer.py
+++ PassthroughTransformer.py
@@ -4,6 +4,7 @@ import numpy as np
 
 class PassthroughTransformer(BaseEstimator):
     def fit(self, X, y=None):
+        # y to support API
         self.cols = X.columns
         return self
 
@@ -11,5 +12,12 @@ class PassthroughTransformer(BaseEstimator):
         self.cols = X.columns
         return X.values
 
-    def get_feature_names_out(self):
-        return np.array(self.cols)
+    def get_feature_names(self, input_features=None):
+        # to support get_feature_names (get_feature_names is deprecated in 1.0 and
+        # will be removed in 1.2. Please use get_feature_names_out instead)
+        return self.get_feature_names_out(input_features)
+
+    def get_feature_names_out(self, input_features=None):
+        if input_features is None:
+            return np.array(self.cols)
+        return input_features
