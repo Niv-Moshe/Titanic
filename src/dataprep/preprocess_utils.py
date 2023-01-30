abc7c030bfcbfdd5c9647e216c95ff67b5d3c73e
NivMoshe2000 <niv.moshe@campus.technion.ac.il>
2023-01-30 17:20:19 +0200

    added docs to all methods in preprocess_utils.py

--- src/dataprep/preprocess_utils.py
+++ src/dataprep/preprocess_utils.py
@@ -1,7 +1,7 @@
 import re
 import pandas as pd
 import numpy as np
-from typing import List
+from typing import List, Tuple
 from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
 from sklearn.linear_model import LinearRegression
 from sklearn.experimental import enable_iterative_imputer  # IterativeImputer doesn't work without this import
@@ -11,8 +11,12 @@ MISSINGNESS_PCT = 0.005
 
 
 def drop_small_missing_data(df: pd.DataFrame, features: List[str]) -> pd.DataFrame:
+    """
+    This method goes over the features with missing values that have very little rows with missing values and will
+    drop them.
+    E.g. dropping rows for features with less than 0.5% of missing values
+    """
     nulls_percent = df.isna().sum() / len(df)
-    # dropping row for features with less than 0.5% of missing values
     small_missing_values_features = [feature for feature in features if 0 < nulls_percent[feature] < MISSINGNESS_PCT]
     for feature in small_missing_values_features:
         df = df[df[feature].notnull()]
@@ -20,10 +24,12 @@ def drop_small_missing_data(df: pd.DataFrame, features: List[str]) -> pd.DataFra
 
 
 def feature_index_for_missing_data(df: pd.DataFrame, features: List[str]) -> pd.DataFrame:
+    """
+    This method creates a new binary column indicating which rows have missing data for each feature.
+    """
     nulls_count = df.isna().sum()
     for feature in features:
         if nulls_count[feature] >= 1:
-            # a binary column indicating which rows have missing data for that feature
             if feature == 'Survived':  # wouldn't happen (full data on independent variable)
                 continue
             missing_feature_name = f"{feature}NaN"
@@ -32,6 +38,9 @@ def feature_index_for_missing_data(df: pd.DataFrame, features: List[str]) -> pd.
 
 
 def find_honorifics(name: str) -> str:
+    """
+    This method extracts the honorifics title for a given name
+    """
     honorific_names = r'(?:Mrs|Mr|Ms|Miss|Master|Don|Rev|Mme|Major|Mlle|Col|Capt|Jonkheer|Countess|Dr)\.?'
     honorific = re.findall(honorific_names, name)
     if not honorific:  # empty list - no honorific found
@@ -43,18 +52,27 @@ def find_honorifics(name: str) -> str:
 
 
 def honorifics_feature_from_name(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    This method creates a new column with the honorifics title for each passenger
+    """
     df['Honorifics'] = df['Name'].apply(lambda name: find_honorifics(name))
     return df
 
 
-def solo_feature_from_parch_sibsp(df, y=None) -> pd.DataFrame:
-    # If both SibSp and Parch are 0 then we say the passenger is by himself on board, meaning he is traveling solo
+def solo_feature_from_parch_sibsp(df) -> pd.DataFrame:
+    """
+    This method creates a new column which indicates if a passenger is traveling solo.
+    We say a passenger is travelling solo if both SibSp and Parch are 0.
+    """
     df['Solo'] = (df['Parch'] + df['SibSp']).apply(lambda x: 1 if x == 0 else 0)
     return df
 
 
 def cabin_char_feature_from_cabin(df: pd.DataFrame) -> pd.DataFrame:
-    # Extracting first character from the cabin (which is with about 70% of missing values in train)
+    """
+    This method creates a new column which holds the first character from the cabin of a passenger
+    (cabin is with about 70% of missing values in train).
+    """
     cabins = df['Cabin'].astype(str)
     cabin_char = [cab[0] for cab in cabins]  # when NaN then char would be 'n'
     df['CabinChar'] = cabin_char
@@ -62,23 +80,39 @@ def cabin_char_feature_from_cabin(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def ticket_group_and_group_size_features_from_ticket(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    This method creates a new columns about information on tickets: Whether it is a group ticket (meaning some
+    passengers have the same ticket) and the group size if it is a group ticket.
+    """
     ticket_counts_dict = df['Ticket'].value_counts().to_dict()
     df['TicketGroup'] = df['Ticket'].apply(lambda x: 1 if ticket_counts_dict[x] > 1 else 0)
     df['GroupSize'] = df['Ticket'].map(ticket_counts_dict)
     return df
 
 
-def label_encoding_transformer(label_encoder_features):
+def label_encoding_transformer(label_encoder_features: List[str]) -> Tuple[str, OrdinalEncoder, List[str]]:
+    """
+    This method returns a tuple of a transformer (OrdinalEncoder) with its name to be in the ColumnTransformer and
+    the features it will work on
+    """
     label_encoder_transformer = OrdinalEncoder()
     return "label_encoder", label_encoder_transformer, label_encoder_features
 
 
-def one_hot_encoding_transformer(one_hot_features):
+def one_hot_encoding_transformer(one_hot_features: List[str]) -> Tuple[str, OneHotEncoder, List[str]]:
+    """
+    This method returns a tuple of a transformer (OneHotEncoder) with its name to be in the ColumnTransformer and
+    the features it will work on
+    """
     one_hot_transformer = OneHotEncoder(handle_unknown="ignore", sparse_output=False)
     return "onehot", one_hot_transformer, one_hot_features
 
 
-def numeric_imputing_transformer(numeric_features):
+def numeric_imputing_transformer(numeric_features: List[str]) -> Tuple[str, IterativeImputer, List[str]]:
+    """
+    This method returns a tuple of a transformer (IterativeImputer) with its name to be in the ColumnTransformer and
+    the features it will work on
+    """
     numeric_transformer = IterativeImputer(estimator=LinearRegression(), missing_values=np.nan, max_iter=10,
                                            imputation_order='roman', random_state=0)
     return "numeric", numeric_transformer, numeric_features
